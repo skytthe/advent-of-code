f3f3bc0bad91401d967504e87aa3363d3b03dbd4
skytthe <skytthe@gmail.com>
2024-10-18 23:33:45 +0200

    2015 day 14 part 2

--- 2015/cpp/day14/day14.cpp
+++ 2015/cpp/day14/day14.cpp
@@ -15,7 +15,20 @@ struct reindeer
     int speed;
     int fly_time;
     int rest_time;
+    int point;
+    int distance;
 };
+
+ostream &operator<<(ostream &os, const reindeer &r)
+{
+    os << "Reindeer Name: " << r.name << endl;
+    os << "   Speed: " << r.speed << " km/h" << endl;
+    os << "   Fly Time: " << r.fly_time << " seconds" << endl;
+    os << "   Rest Time: " << r.rest_time << " seconds" << endl;
+    os << "   Points: " << r.point << endl;
+    os << "   Distance: " << r.distance << " km" << endl;
+    return os;
+}
 
 int main()
 {
@@ -28,6 +41,7 @@ int main()
     }
 
     int duration = 2503;
+    //    duration = 1000;
     vector<reindeer> reindeers;
 
     string line;
@@ -40,7 +54,7 @@ int main()
         {
             list.push_back(tmp);
         }
-        reindeer r = {list[0], stoi(list[3]), stoi(list[6]), stoi(list[13])};
+        reindeer r = {list[0], stoi(list[3]), stoi(list[6]), stoi(list[13]), 0, 0};
         reindeers.push_back(r);
     }
 
@@ -52,10 +66,9 @@ int main()
     //     cout << "Rest Time: " << hans.rest_time << " seconds" << endl
     //          << endl;
     // }
-
     vector<int> travel_distances(reindeers.size());
     int max_travel_distances = 0;
-    for (auto r : reindeers)
+    for (auto &r : reindeers)
     {
         int divisor = duration / (r.fly_time + r.rest_time);
         int remainder = duration % (r.fly_time + r.rest_time);
@@ -68,6 +81,43 @@ int main()
 
     cout << "Part 1:" << endl
          << max_travel_distances << endl;
+
+    // PART 2
+    int itr_max_distance = 0;
+    for (int i = 1; i <= duration; i++)
+    {
+        // cout << "-----------------" << endl;
+        // cout << "-----------------" << endl;
+        // cout << "step: " << i << endl;
+        // cout << "-----------------" << endl;
+        for (auto &r : reindeers)
+        {
+            int remainder = i % (r.fly_time + r.rest_time);
+            r.distance += (remainder != 0 && remainder <= r.fly_time) ? r.speed : 0;
+            itr_max_distance = max(itr_max_distance, r.distance);
+        }
+        // cout << endl;
+        // cout << itr_max_distance << endl;
+        // cout << "-----------------" << endl;
+        for (auto &r : reindeers)
+        {
+            if (r.distance == itr_max_distance)
+            {
+                r.point++;
+            }
+            // cout << r << endl;
+        }
+        itr_max_distance = 0;
+    }
+
+    int max_point = -1;
+    for (auto &r : reindeers)
+    {
+        //        cout << r << endl;
+        max_point = max(max_point, r.point);
+    }
+    cout << "Part 2:" << endl
+         << max_point << endl;
 
     return 0;
 }
