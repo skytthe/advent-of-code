1744700fe5a3afd5cc9f31b2af37d7d47fcab5a7
Stefan Larsen <skytthe@gmail.com>
2024-10-23 00:24:33 +0200

    2015 day 19 part 2, too slow for big inputs

--- 2015/cpp/day19/day19.cpp
+++ 2015/cpp/day19/day19.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 int main()
 {
-    ifstream file("2015/inputs/day19.txt");
+    ifstream file("2015/inputs/day19_s.txt");
     if (!file.is_open())
     {
         cerr << "Error: Could not open the file." << endl;
@@ -49,7 +49,7 @@ int main()
 
     string molecule = line;
 
-    set<string> new_molecules;
+        set<string> new_molecules;
     string key;
     string left, right, tmp;
     for (int i = 0; i < molecule.size(); i++)
@@ -69,7 +69,7 @@ int main()
             for (auto e : idx->second)
             {
                 tmp = left + e + right;
-                cout << tmp << endl;
+//                cout << tmp << endl;
                 new_molecules.insert(tmp);
             }
         }
@@ -78,6 +78,55 @@ int main()
     std::cout << "Part 1:" << std::endl
               << new_molecules.size() << std::endl;
 
+
+    // Part 2
+    string medicine = molecule;
+    molecule = "e";
+
+    set<string> molecules;
+    molecules.insert(molecule);
+
+    new_molecules.clear();
+    int steps = 0;
+    while (true)
+    {
+        steps++;
+        for (auto m: molecules)
+        {
+            for (int i = 0; i < m.size(); i++)
+            {
+                left = m.substr(0,i);
+                key = m[i];
+                if (i < m.size() && islower(m[i+1]))
+                {
+                    key += m[i+1];
+                    i++;
+                }
+                right = m.substr(i+1);
+
+                auto idx = reaction_map.find(key);
+                if (idx != reaction_map.end())
+                {
+                    for (auto e : idx->second)
+                    {
+                        tmp = left + e + right;
+                        new_molecules.insert(tmp);
+                    }
+                }
+            }
+        }
+        
+
+        if(new_molecules.count(medicine)){
+            break;
+        }
+        molecules = new_molecules;
+        new_molecules.clear();
+
+    }
+    
+    std::cout << "Part 2:" << std::endl
+              << steps << std::endl;
 
     file.close();
 
