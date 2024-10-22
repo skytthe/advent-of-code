3dbb9e03f4fc5fed0ad611cda8e7d6357de20086
Stefan Larsen <skytthe@gmail.com>
2024-10-22 23:48:59 +0200

    2015 day 19 part 1

--- 2015/cpp/day19/day19.cpp
+++ 2015/cpp/day19/day19.cpp
@@ -5,8 +5,11 @@
 #include <unordered_map>
 #include <vector>
 #include <map>
+#include <set>
 #include <numeric>
 #include <algorithm>
+#include <climits>
+#include <cctype>
 
 using namespace std;
 
@@ -44,7 +47,37 @@ int main()
     }
     getline(file, line);
 
-    string input_molecule = line;
+    string molecule = line;
+
+    set<string> new_molecules;
+    string key;
+    string left, right, tmp;
+    for (int i = 0; i < molecule.size(); i++)
+    {
+        left = molecule.substr(0,i);
+        key = molecule[i];
+        if (i < molecule.size() && islower(molecule[i+1]))
+        {
+            key += molecule[i+1];
+            i++;
+        }
+        right = molecule.substr(i+1);
+
+        auto idx = reaction_map.find(key);
+        if (idx != reaction_map.end())
+        {
+            for (auto e : idx->second)
+            {
+                tmp = left + e + right;
+                cout << tmp << endl;
+                new_molecules.insert(tmp);
+            }
+        }
+    }
+    
+    std::cout << "Part 1:" << std::endl
+              << new_molecules.size() << std::endl;
+
 
     file.close();
 
