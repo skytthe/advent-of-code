a86a77fa9d5849413422ffa9434edc5497078ef3
skytthe <skytthe@gmail.com>
2024-11-17 20:23:13 +0100

    2015 day 8 part 2

--- 2015/cpp/day08/day08.cpp
+++ 2015/cpp/day08/day08.cpp
@@ -24,11 +24,13 @@ int main()
 
     int sumMemory = 0;
     int sumCharacters = 0;
+    int sumEncoding = 0;
 
     string line;
     while (getline(file, line))
     {
         sumMemory += line.size();
+        sumEncoding += line.size() + 4;
 
         int count = 0;
         for (int i = 1; i < line.size() - 1; i++)
@@ -39,20 +41,26 @@ int main()
                 if (line.at(i + 1) == 'x')
                 {
                     i = i + 3;
+                    sumEncoding += 1;
                 }
                 else
                 {
                     i = i + 1;
+                    sumEncoding += 2;
                 }
             }
         }
         sumCharacters += count;
     }
 
-    int total = sumMemory - sumCharacters;
+    int total1 = sumMemory - sumCharacters;
+    int total2 = sumEncoding - sumMemory;
 
     std::cout << "Part 1:" << std::endl
-              << total << std::endl;
+              << total1 << std::endl;
+
+    std::cout << "Part 2:" << std::endl
+              << total2 << std::endl;
 
     file.close();
     return 0;
