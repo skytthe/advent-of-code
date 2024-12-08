5f2f433ae00a024e81a33b479628cdeb4a4551d4
skytthe <skytthe@gmail.com>
2024-12-08 22:14:18 +0100

    2024 day 8

--- 2024/py/day08/day08.py
+++ 2024/py/day08/day08.py
@@ -28,7 +28,8 @@ row = len(data)
 col = len(data[0])
 
 antennas = defaultdict(list)
-antinodes = set()
+antinodes1 = set()
+antinodes2 = set()
 
 for y, l in enumerate(data):
     for x, e in enumerate(l):
@@ -38,12 +39,19 @@ for y, l in enumerate(data):
 for k in antennas:
     for a, b in combinations(antennas[k], 2):
         dx, dy = b[0]-a[0], b[1]-a[1]
+        antinodes1.add((a[0]-dx, a[1]-dy))
+        antinodes1.add((b[0]+dx, b[1]+dy))
         for i in range(-50, 50):
-            antinodes.add((a[0]-(dx*i), a[1]-(dy*i)))
-            antinodes.add((b[0]+(dx*i), b[1]+(dy*i)))
+            antinodes2.add((a[0]-(dx*i), a[1]-(dy*i)))
+            antinodes2.add((b[0]+(dx*i), b[1]+(dy*i)))
 
-for x, y in antinodes:
+for x, y in antinodes1:
+    if x >= 0 and y >= 0 and x < col and y < row:
+        ans1 += 1
+
+for x, y in antinodes2:
     if x >= 0 and y >= 0 and x < col and y < row:
         ans2 += 1
 
+print(ans1)
 print(ans2)
