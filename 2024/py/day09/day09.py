706be3141eae8e62d7e992208f86702a6c09da01
skytthe <skytthe@gmail.com>
2024-12-09 22:44:27 +0100

    2024 day 9 part 2

--- 2024/py/day09/day09.py
+++ 2024/py/day09/day09.py
@@ -9,14 +9,6 @@ with open('2024/py/day09/day09.txt') as f:
 example = [int(x) for x in example[0]]
 lines = [int(x) for x in lines[0]]
 
-print(example)
-print(sum(example))
-print(len(example))
-print()
-
-print(sum(lines))
-print(len(lines))
-
 
 def printMem(mem):
     s = ""
@@ -28,38 +20,83 @@ def printMem(mem):
     print(s)
 
 
+def printMem2(mem):
+    s = ""
+    for m in mem:
+        if m[1] == -1:
+            s += '.' * m[0]
+        else:
+            s += str(m[1]) * m[0]
+    print(s)
+
+
 data = example
 data = lines
 
-m = [-1] * sum(data)
+m1 = [-1] * sum(data)
+m2 = []
 
 id = 0
 idx = 0
 for i, d in enumerate(data):
     if i % 2 == 0:
-        m[idx:(idx+d)] = [id] * d
+        m1[idx:(idx+d)] = [id] * d
         idx += d
+        m2.append([d, id])
         id += 1
     else:
         idx += d
+        m2.append([d, -1])
+
 
 i1 = 0
-i2 = len(m)-1
-# printMem(m)
+i2 = len(m1)-1
+# printMem(m1)
 while True:
-    while (m[i1] != -1):
+    while (m1[i1] != -1):
         i1 += 1
     if i1 >= i2:
         break
-    m[i1], m[i2] = m[i2], m[i1]
-    while (m[i2] == -1):
+    m1[i1], m1[i2] = m1[i2], m1[i1]
+    while (m1[i2] == -1):
         i2 -= 1
 
-    # printMem(m)
+    # printMem(m1)
 
 ans1 = 0
-for i, e in enumerate(m):
+for i, e in enumerate(m1):
     if e != -1:
         ans1 += i*e
 
 print(ans1)
+
+# printMem2(m2)
+for i in reversed(range(len(m2))):
+    if m2[i][1] == -1:
+        continue
+    for j in range(i):
+        if m2[j][1] == -1:
+            if m2[j][0] == m2[i][0]:
+                m2[j], m2[i] = m2[i], m2[j]
+
+                # printMem2(m2)
+                break
+            elif m2[j][0] > m2[i][0]:
+                tmp = m2[i]
+                m2[i] = [m2[i][0], -1]
+                padding = [(m2[j][0]-m2[i][0]), -1]
+                m2[j] = tmp
+                m2.insert((j+1), padding)
+
+                # printMem2(m2)
+                break
+
+ans2 = 0
+itr = 0
+for e in m2:
+    for j in range(e[0]):
+        if e[1] != -1:
+            ans2 += itr*e[1]
+        itr += 1
+
+print(ans2)
