92aac16a9d8db526b04506ecbce2d6ae60f2c09b
skytthe <skytthe@gmail.com>
2024-12-11 00:28:19 +0100

    2020 day 1 part 1

---
+++ 2020/py/day01/day01.py
@@ -0,0 +1,28 @@
+import sys
+from itertools import combinations
+
+example = """1721
+979
+366
+299
+675
+1456""".split("\n")
+
+with open('2020/py/day01/day01.txt') as f:
+    lines = [line.strip() for line in f.readlines()]
+
+data = example
+data = lines
+data = [int(x) for x in data]
+
+ans11 = 0
+for a, b in combinations(data, 2):
+    if a + b == 2020:
+        ans11 = a * b
+        break
+
+ans12 = next((a * b for a, b in combinations(data, 2) if a + b == 2020), -1)
+
+assert ans11 == ans12
+
+print(ans11)
