8be33a8a7de264811e7f491ab7dd30b529d74773
skytthe <skytthe@gmail.com>
2024-12-11 10:13:55 +0100

    2024 day 11 part 1

---
+++ 2024/py/day11/day11.py
@@ -0,0 +1,38 @@
+from collections import Counter
+from functools import lru_cache
+
+
+example = """125 17""".strip()
+
+with open('2024/py/day11/day11.txt') as f:
+    s = f.read().strip()
+    # s = [line.strip() for line in f.readlines()]
+
+data = example
+data = s
+
+data = [int(x) for x in data.split()]
+
+
+def blink(count):
+    newCount = Counter()
+    for stone, n in count.items():
+        if stone == 0:
+            newCount[1] += n
+        else:
+            s = str(stone)
+            if len(s) % 2 == 0:
+                idx = len(s)//2
+                newCount[int(s[:idx])] += n
+                newCount[int(s[idx:])] += n
+            else:
+                newCount[stone * 2024] += n
+    return newCount
+
+
+count = Counter(data)
+for i in range(25):
+    count = blink(count)
+
+
+print(sum(count.values()))
