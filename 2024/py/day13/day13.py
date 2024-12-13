9ee4891b98640fdf8a47ddbacb184e42e0e08839
skytthe <skytthe@gmail.com>
2024-12-13 14:08:13 +0100

    2024 day 13 part 2

--- 2024/py/day13/day13.py
+++ 2024/py/day13/day13.py
@@ -4,8 +4,21 @@ import sys
 import queue
 import re
 
-example = """
-""".split("\n")
+example = """Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279"""
 
 with open('2024/py/day13/day13.txt') as f:
     s = f.read().strip()
@@ -14,7 +27,7 @@ with open('2024/py/day13/day13.txt') as f:
 data = example
 data = s
 
-data = [[z] for z in data.split("\n\n")]
+data = [[x] for x in data.split("\n\n")]
 
 games = [[int(num) for num in re.findall(r"\d+", x[0])] for x in data]
 
@@ -40,3 +53,30 @@ for g in games:
         ans1 += int(result.fun)
 
 print(ans1)
+
+
+ans2 = []
+
+for g in games:
+    x1, y1, x2, y2, gx, gy = g
+
+    c = [3, 1]
+    A_eq = [
+        [x1, x2],
+        [y1, y2],
+    ]
+    b_eq = [10000000000000+gx, 10000000000000+gy]
+
+    bounds = [(0, None), (0, None)]
+
+    result = linprog(c, A_eq=A_eq, b_eq=b_eq, bounds=bounds)
+
+    if result.success:
+        a, b = result.x
+        if (round(a) * x1 + round(b) * x2 == 10000000000000+gx) and (round(a) * y1 + round(b) * y2 == 10000000000000+gy):
+            # print(g)
+            ans2.append(round(result.fun))
+
+# print(len(games))
+# print(len(ans2))
+print(sum(ans2))
