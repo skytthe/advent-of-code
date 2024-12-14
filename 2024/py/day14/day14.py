4d187f761076ef6c7f0c94bea5157218ce992824
skytthe <skytthe@gmail.com>
2024-12-14 07:07:51 +0100

    2024 day 14 part 1

---
+++ 2024/py/day14/day14.py
@@ -0,0 +1,65 @@
+import sys
+import re
+import pprint
+
+example = """p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3""".split("\n")
+
+
+# example = """p=2,4 v=2,-3""".split("\n")
+
+with open('2024/py/day14/day14.txt') as f:
+    s = [line.strip() for line in f.readlines()]
+
+data = example
+row = 7
+col = 11
+data = s
+row = 103
+col = 101
+
+
+robots = [[int(num) for num in re.findall(r"-?\d+", x)] for x in data]
+
+# print(robots)
+
+grid = [[0 for x in range(col)]for y in range(row)]
+
+s = 100
+
+grid = [[0 for x in range(col)]for y in range(row)]
+for r in robots:
+    x = (r[0] + s * r[2]) % col
+    y = (r[1] + s * r[3]) % row
+    grid[y][x] += 1
+# pprint.pp(grid)
+# print()
+
+
+q1 = [row[:col//2] for row in grid[:row//2]]
+q2 = [row[col//2+1:] for row in grid[:row//2]]
+q3 = [row[:col//2] for row in grid[row//2+1:]]
+q4 = [row[col//2+1:] for row in grid[row//2+1:]]
+
+
+# pprint.pprint(q1)
+# pprint.pprint(q2)
+# pprint.pprint(q3)
+# pprint.pprint(q4)
+
+
+ans1 = sum(sum(row) for row in q1) * sum(sum(row) for row in q2) * \
+    sum(sum(row) for row in q3) * sum(sum(row) for row in q4)
+
+
+print(ans1)
