e66aa3587ea4ae2faf7a61072b7620a242534a0d
skytthe <skytthe@gmail.com>
2024-12-22 10:11:01 +0100

    2024 day 22 part 2

--- 2024/py/day22/day22.py
+++ 2024/py/day22/day22.py
@@ -1,15 +1,23 @@
 import sys
 import math
+from collections import defaultdict
+import pprint
 
 example = """1
 10
 100
 2024""".split("\n")
 
+example2 = """1
+2
+3
+2024""".split("\n")
+
 with open('2024/py/day22/day22.txt') as f:
     lines = [line.strip() for line in f.readlines()]
 
 data = example
+data = example2
 data = lines
 
 initialSecretNumbers = list(map(int, data))
@@ -35,10 +43,26 @@ def nextSecretNumber(n):
 
 ans1 = 0
 
+sequences = defaultdict(list)
+
 for sn in initialSecretNumbers:
     secretNumber = sn
+    seen = set()
+    changes = []
+    prev = sn % 10
     for _ in range(2000):
         secretNumber = nextSecretNumber(secretNumber)
+        banana = secretNumber % 10
+        changes.append(banana-prev)
+        prev = banana
+        if len(changes) >= 4:
+            tmp = tuple(changes[-4:])
+            if tmp not in seen:
+                sequences[tmp].append(banana)
+                seen.add(tmp)
     ans1 += secretNumber
 
+ans2 = max([(sum(s)) for _, s in sequences.items()])
+
 print(ans1)
+print(ans2)
