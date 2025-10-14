0245e2dc262ab2d3c14a6cbe5bbdb4cc10db6f9a
Stefan Larsen <skytthe@gmail.com>
2025-10-14 12:09:27 +0200

    2021 day 8 part 1

---
+++ 2021/py/day08/day08.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+example = """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce""".split("\n")
+
+with open("2021/py/day08/day08.txt") as f:
+    lines = [line.strip() for line in f.readlines()]
+
+data = example
+data = lines
+
+data = [[part.split() for part in line.split(" | ")] for line in data]
+
+
+right = Counter([len(item) for line in data for item in line[1]])
+print(right[2] + right[3] + right[4] + right[7])
+
