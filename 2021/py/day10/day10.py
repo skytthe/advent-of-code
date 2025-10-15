0ec3726b4dd28b2eedab67682d484cf98d60fe33
Stefan Larsen <skytthe@gmail.com>
2025-10-15 17:30:46 +0200

    2021 day 10 part 2

--- 2021/py/day10/day10.py
+++ 2021/py/day10/day10.py
@@ -15,16 +15,26 @@ with open("2021/py/day10/day10.txt") as f:
 data = example
 data = lines
 
-cost = {
+cost1 = {
     ")": 3,
     "]": 57,
     "}": 1197,
     ">": 25137,
 }
 
+cost2 = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
+}
+
+
 errors = []
+scores = []
 for line in data:
     stack = []
+    flag = True
     for ch in line:
         if ch in {'(','[','{','<',}:
             stack.append(ch)
@@ -39,7 +49,27 @@ for line in data:
                 stack.pop()
             else:
                 errors.append(ch)
+                flag = False
                 break
+    if flag:
+        score = 0
+        for ch in reversed(stack):
+            try:
+                score = score * 5 + cost2[chr(ord(ch)+1)]
+            except:
+                score = score * 5 + cost2[chr(ord(ch)+2)]
+        scores.append(score)
 
-total = sum([cost[i] for i in errors])
+
+total = sum([cost1[i] for i in errors])
 print(total)
+
+scores.sort()
+print(scores[len(scores)//2])
+
+
+# print(ord("{"))
+# print(chr(ord("(")+1))
+# print(chr(ord("[")+2))
+# print(chr(ord("{")+2))
+# print(chr(ord("<")+2))
