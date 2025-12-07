0e48ee0327ef7600555ac5e802f3065547c736e6
Stefan Larsen <skytthe@gmail.com>
2025-12-07 13:02:30 +0100

    2025 day 7

--- 2025/py/day07/day07.py
+++ 2025/py/day07/day07.py
@@ -1,5 +1,4 @@
 from collections import Counter
-import pprint
 
 example = """.......S.......
 ...............
@@ -26,23 +25,7 @@ data = lines
 
 grid = [list(line) for line in data[1:]]
 
-start1 = data[0].find("S")
-beams1 = [{start1}]
-splits = 0
-
-for row in grid:
-    newBeams = set()
-    for b in beams1[-1]:
-        if row[b] == "^":
-            splits += 1
-            newBeams.add(b-1)
-            newBeams.add(b+1)
-        else:
-            newBeams.add(b)
-    beams1.append(newBeams)
-
-print(splits)
-
+splits1 = 0
 
 start2 = data[0].find("S")
 beams2 = [Counter([start2])]
@@ -51,10 +34,12 @@ for i, row in enumerate(grid):
     newBeams2 = Counter()
     for b, count in beams2[-1].items():
         if row[b] == "^":
+            splits1 += 1
             newBeams2[b-1] += count
             newBeams2[b+1] += count
         else:
             newBeams2[b] += count
     beams2.append(newBeams2)
 
+print(splits1)
 print(sum(beams2[-1].values()))
