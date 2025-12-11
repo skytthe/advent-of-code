58f79e95f6da3fd173a10ce53231225d26e39bfe
Stefan Larsen <skytthe@gmail.com>
2025-12-11 07:49:09 +0100

    2025 day 11 part 2

--- 2025/py/day11/day11.py
+++ 2025/py/day11/day11.py
@@ -1,5 +1,6 @@
 import sys
 from collections import deque, defaultdict
+from functools import cache
 
 example = """aaa: you hhh
 you: bbb ccc
@@ -12,10 +13,25 @@ ggg: out
 hhh: ccc fff iii
 iii: out""".split("\n")
 
+example2 = """svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out""".split("\n")
+
 with open('2025/py/day11/day11.txt') as f:
     lines = [line.strip() for line in f.readlines()]
 
 data = example
+data = example2
 data = lines
 
 
@@ -38,5 +54,26 @@ while q:
         else:
             q.append(e)
 
-
 print(paths)
+
+
+@cache
+def travelGraph(node, dac, fft):
+    global graph
+    sum = 0
+    for e in graph[node]:
+        if e == "out":
+            if dac and fft:
+                return 1
+            else: 
+                return 0
+        else:
+            if e == "dac":
+                sum += travelGraph(e, True, fft)
+            elif e == "fft":
+                sum += travelGraph(e, dac, True)
+            else:               
+                sum += travelGraph(e, dac, fft)
+    return sum
+
+print(travelGraph("svr", False, False))
