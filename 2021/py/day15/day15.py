f78fc7161e025054653b5d2ba319f5c97aa918a5
Stefan Larsen <skytthe@gmail.com>
2025-12-13 17:20:24 +0100

    2021 day 15 part 2

--- 2021/py/day15/day15.py
+++ 2021/py/day15/day15.py
@@ -18,36 +18,45 @@ data = example
 data = lines
 
 grid = [list(map(int, line)) for line in data]
-
 R = len(grid)
 C = len(grid[0])
+grid2 = [[0]*C*5 for _ in range(R*5)]
+for yy in range(5):
+    for xx in range(5):
+        for y in range(R):
+            for x in range(C):
+                grid2[yy*R+y][xx*C+x] = (grid[y][x] + yy + xx - 1) % 9 + 1
 
-moves = ((1,0),(-1,0),(0,1),(0,-1))
+def fincPath(grid):
+    R = len(grid)
+    C = len(grid[0])
 
-start = (0,0)
-goal = (C-1,R-1)
+    moves = ((1,0),(-1,0),(0,1),(0,-1))
 
-visited = set()
+    start = (0,0)
+    goal = (C-1,R-1)
 
-q = []
-heapq.heappush(q, (0, start))
+    visited = set()
 
-while q:
-    cost, xy = heapq.heappop(q)
-    
-    if xy in visited:
-        continue
-    visited.add(xy)
-    
-    if xy == goal:
-        print(cost)
-        break
+    q = []
+    heapq.heappush(q, (0, start))
 
-    x,y = xy
-    for dx,dy in moves:
-        nx,ny = x+dx, y+dy
-        if (nx,ny) not in visited and 0 <= nx < C and 0 <= ny < R:
-            heapq.heappush(q, (cost+grid[ny][nx], (nx,ny)))
+    while q:
+        cost, xy = heapq.heappop(q)
+        
+        if xy in visited:
+            continue
+        visited.add(xy)
+
+        if xy == goal:
+            return cost
+
+        x,y = xy
+        for dx,dy in moves:
+            nx,ny = x+dx, y+dy
+            if (nx,ny) not in visited and 0 <= nx < C and 0 <= ny < R:
+                heapq.heappush(q, (cost+grid[ny][nx], (nx,ny)))
 
 
-
+print(fincPath(grid))
+print(fincPath(grid2))
