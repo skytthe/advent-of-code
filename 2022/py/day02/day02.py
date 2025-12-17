3daab9fd07af15cbea2c70d284387edc428dec3d
Stefan Larsen <skytthe@gmail.com>
2025-12-17 08:09:34 +0100

    2022 day 2 part 2

--- 2022/py/day02/day02.py
+++ 2022/py/day02/day02.py
@@ -26,11 +26,25 @@ outcomes = {
     "CY" : 0,
     "CZ" : 3,
 }
-
+# X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
+strategy = {
+    "AX" : "Z",
+    "AY" : "X",
+    "AZ" : "Y",
+    "BX" : "X",
+    "BY" : "Y",
+    "BZ" : "Z",
+    "CX" : "Y",
+    "CY" : "Z",
+    "CZ" : "X",
+}
 
 ans1 = 0
+ans2 = 0
 for p1,p2 in data:
     ans1 += shapes[p2] + outcomes[p1+p2]
+    ans2 += shapes[strategy[p1+p2]] + outcomes[p1+strategy[p1+p2]]
 
 print(ans1)
-
+# print(ans2)
+print(sum([shapes[strategy[p1+p2]] + outcomes[p1+strategy[p1+p2]] for p1,p2 in data]))
