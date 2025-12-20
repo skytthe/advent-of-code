972f2763335910cca4388dd21fb8dff14af1042e
Stefan Larsen <skytthe@gmail.com>
2025-12-20 15:04:56 +0100

    2022 day 6 part 2

--- 2022/py/day06/day06.py
+++ 2022/py/day06/day06.py
@@ -12,10 +12,14 @@ data = lines
 
 
 ans1 = None
+ans2 = None
 for i in range(len(data)-4):
-    if len(set(data[i:i+4])) == 4:
+    if len(set(data[i:i+4])) == 4 and ans1 == None:
         ans1 = i+4
+    if len(set(data[i:i+14])) == 14:
+        ans2 = i+14
         break
 
-assert ans1 != None
+assert ans1 != None and ans2 != None
 print(ans1)
+print(ans2)
