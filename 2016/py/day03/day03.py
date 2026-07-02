6cbafe356913d121e44ca0e46bb0975544360b7e
Stefan Larsen <skytthe@gmail.com>
2026-07-02 23:16:17 +0200

    2016 day 3 part 2

--- 2016/py/day03/day03.py
+++ 2016/py/day03/day03.py
@@ -10,8 +10,6 @@ with open('2016/py/day03/day03.txt') as f:
 data = example
 data = lines
 
-print(data)
-
 count = 0
 for line in data:
     tmp = re.findall(r'\d+', line)
@@ -20,3 +18,13 @@ for line in data:
         count += 1
 
 print(count)
+
+
+data2 = np.array([re.findall(r'\d+', line) for line in data],dtype=int).T.reshape(-1,3)
+
+count2 = 0
+for tmp in data2:
+    if tmp[0] + tmp[1] > tmp[2] and tmp[0] + tmp[2] > tmp[1] and tmp[1] + tmp[2] > tmp[0]:
+        count2 += 1
+
+print(count2)
