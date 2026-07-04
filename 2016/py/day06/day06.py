0e2ab7e03d2ff335c7b293fecc2c0d3b894cb899
Stefan Larsen <skytthe@gmail.com>
2026-07-04 19:38:13 +0200

    2016 day 6 part 1

---
+++ 2016/py/day06/day06.py
@@ -0,0 +1,38 @@
+import re
+import numpy as np
+from collections import defaultdict, Counter
+
+example = """eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar""".splitlines()
+
+with open('2016/py/day06/day06.txt') as f:
+    lines = [line.strip() for line in f.readlines()]
+
+data = example
+data = lines
+
+counters = [Counter() for _ in range(len(data[0]))]
+
+for line in data:
+    for n, ch in enumerate(line):
+        counters[n].update(ch)
+
+ans = ""
+for c in counters:
+    ans += c.most_common()[0][0]
+
+print(ans)
