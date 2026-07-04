177c73de3ba96b652d065bf2966011e97bae95aa
Stefan Larsen <skytthe@gmail.com>
2026-07-04 12:33:59 +0200

    2016 day 5 part 2

--- 2016/py/day05/day05.py
+++ 2016/py/day05/day05.py
@@ -18,3 +18,20 @@ while len(pw) < 8:
 
 
 print(pw)
+
+
+pw2 = " " * 8
+idx = 0
+flags = [True]*8
+while any(flags):
+    tmp = hashlib.md5((data + str(idx)).encode()).hexdigest()
+    idx += 1
+    if tmp[:5] == "00000" and tmp[5].isdigit():
+        i = int(tmp[5])
+        if 0 <= i < 8 and flags[i]:
+            pw2 = pw2[:i] + tmp[6] + pw2[i+1:]
+            # print(pw2)
+            flags[i] = False
+
+
+print(pw2)
