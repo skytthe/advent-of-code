44b9a2fd2ec9fde3639eff85bc7065c5a2502205
Stefan Larsen <skytthe@gmail.com>
2026-07-05 14:06:38 +0200

    2016 day 7 part 2

--- 2016/py/day07/day07.py
+++ 2016/py/day07/day07.py
@@ -7,10 +7,16 @@ abcd[bddb]xyyx
 aaaa[qwer]tyui
 ioxxoj[asdfgh]zxcvbn""".splitlines()
 
+example2 = """aba[bab]xyz
+xyx[xyx]xyx
+aaa[kek]eke
+zazbz[bzb]cdb""".splitlines()
+
+
 with open('2016/py/day07/day07.txt') as f:
     lines = [line.strip() for line in f.readlines()]
 
-data = example
+data = example2
 data = lines
 
 ans = 0
@@ -35,3 +41,34 @@ for line in data:
         ans += 1
 
 print(ans)
+
+
+ans2 = 0
+for line in data:
+    aba = set()
+    bab = []
+    hypernet = False
+    idx = 0
+    for n in range(len(line)-2):
+        if line[n] == "[":
+            hypernet = True
+            idx = n+1
+            continue
+        if line[n] == "]":
+            hypernet = False
+            bab.append(line[idx:n])
+            continue
+        if not hypernet:
+            if line[n] != line[n+1] and line[n] == line[n+2]:
+                aba.add(line[n:n+3])
+
+    flag = False
+    for s in aba:
+        tmp = s[1]+s[0]+s[1]
+        for hn in bab:
+            if hn.count(tmp) > 0:
+                flag = True
+    if flag:
+        ans2 += 1
+
+print(ans2)
